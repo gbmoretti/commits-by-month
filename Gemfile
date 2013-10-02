27885b657fdcf5560f0b94133928a432bac3e1ad
Guilherme Moretti <guilherme.moretti@innvent.com.br>
2013-10-02 14:56:49 -0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'octokit', "~> 2.0"
+gem 'pry-rails'
+gem 'simplecov'
---
+++ main.rb
@@ -0,0 +1,67 @@
+require 'octokit'
+require 'pry'
+require 'yaml'
+
+class GitHubAuth
+  # Change NOTE, SCOPES and CREDENTIALS to match your app's needs.
+  NOTE = "commits_monthly"
+  SCOPES = ["user", "repo", "repo:status"]
+  CREDENTIALS = File.join("#{ENV['HOME']}", ".config", "sample_app.yml")
+ 
+  def self.client
+    new.client
+  end
+ 
+  def client
+    @client ||= lambda do
+      unless File.exist?(CREDENTIALS)
+        authenticate
+      end
+      Octokit::Client.new(YAML.load_file(CREDENTIALS))
+    end.call
+  end
+ 
+  private
+  def authenticate
+    login = password = token = ""
+    login = `git config github.user`.chomp
+    login = ask_login if login.empty?
+    password = ask_password
+    auth_client = Octokit::Client.new(:login => login, :password => password)
+    auth = auth_client.authorizations.detect { |a| a.note == NOTE }
+    unless auth
+      auth = auth_client.create_authorization(:scopes => SCOPES, :note => NOTE)
+    end
+    File.open(CREDENTIALS, 'w') do |f|      
+      f.puts({ :login => login, :oauth_token => auth.token }.to_yaml)
+    end
+  end
+ 
+  def ask_login
+    p "Enter your GitHub username"
+    gets.chomp
+  end
+ 
+  # No-echo password input, stolen from Defunkt's `hub`
+  # Won't work in Windows
+  def ask_password
+    p "Enter your GitHub password (this will NOT be stored)"
+    tty_state = `stty -g`
+    system 'stty raw -echo -icanon isig' if $?.success?
+    pass = ''
+    while char = $stdin.getbyte and not (char == 13 or char == 10)
+      if char == 127 or char == 8
+        pass[-1,1] = '' unless pass.empty?
+      else
+        pass << char.chr
+      end
+    end
+    pass
+  ensure
+    system "stty #{tty_state}" unless tty_state.empty?
+  end
+end
+
+client = GitHubAuth.client
+
+client.repositories('innvent').each { |x| puts x.full_name }
