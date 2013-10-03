b7bca5da56b10d29a7b3da3954f108e563c4c247
Guilherme Moretti <guilherme.moretti@innvent.com.br>
2013-10-03 12:03:42 -0300

    objects\! objects\! objects\!

--- main.rb
+++ main.rb
@@ -1,44 +1,100 @@
 require 'json'
 require 'httparty'
 
-def request(url,method=Net::HTTP::Get)
-  request = HTTParty::Request.new(method,url,basic_auth: {username: 'gbmoretti', password: '4215891'})
-  response = request.perform
-  if response.code != 200
-    puts "#{response.code}: #{response.message} (#{url})"
-    return nil
-  else
-    response.body
+class Client
+  class BadResponseError < StandardError; end
+  class InvalidBodyError < StandardError; end
+
+  def initialize(username,password)
+    @username = username
+    @password = password
+    @method = Net::HTTP::Get
   end
+
+  def request(url)
+    begin
+      perform_request(url)
+      parse_response
+    rescue BadResponseError => e
+      puts '!! ' + e.message
+      return []
+    rescue InvalidBodyError => e
+      puts '!! ' + e.message
+      return []
+    end
+  end
+
+  private
+  def perform_request(url)
+    @request = HTTParty::Request.new(@method,url,basic_auth: {username: @username, password: @password})
+    @response = @request.perform
+    raise BadResponseError.new("#{@response.code}: #{@response.message} (#{url})") if @response.code != 200
+  end
+
+  def parse_response
+    begin
+      JSON::parse(@response.body)
+    rescue => e
+       raise InvalidBodyError.new(@response.body)
+    end
+  end
+
 end
 
-def parse(string)
-  return [] if string.nil?
-  begin
-    JSON::parse(string)
-  rescue => e
-     puts "Algo deu errado :("
-     puts string
-     raise Exception
+class Repository
+
+  attr_reader :name, :commits
+
+  def initialize(name,client)
+    @name = name
+    @client = client
   end
+
+  def commits_from_author(author)
+    @commits ||= get_commits(author)
+  end
+
+  private
+  def get_commits(author)
+    @client.request("https://api.github.com/repos/#{@name}/commits?author=#{author}")
+  end
+
 end
 
-orgs = %w(elogroup innvent)
+class Organization
+
+  attr_reader :name, :repositories
+
+  def initialize(name,client)
+    @name = name
+    @client = client
+  end
+
+  def repositories
+    @repositories ||= get_repos
+  end
+
+  private
+  def get_repos
+    repositories = []
+    response = @client.request("https://api.github.com/orgs/#{@name}/repos")
+    response.each do |r|
+      repositories << Repository.new(r['full_name'],@client)
+    end
+    repositories
+  end
+
+end
+
+client = Client.new('gbmoretti','4215891')
+orgs = [Organization.new('elogroup',client), Organization.new('innvent',client)]
+
 author = 'gbmoretti'
 
+
 orgs.each do |org|
-  #repositories = exec("curl -s -u gbmoretti:4215891 ")
-  repositories = request("https://api.github.com/orgs/#{org}/repos")
-  repositories = parse(repositories)
-
-
-  repositories.each do |r|
-    full_name =  r['full_name']
-    #cmd = "curl -s -u gbmoretti:4215891 https://api.github.com/repos/#{r['full_name']}/commits?author=#{author}"
-    #commits = exec(cmd)
-    commits = request("https://api.github.com/repos/#{r['full_name']}/commits?author=#{author}")
-
-    commits = parse(commits)
-    puts full_name + ': ' + commits.count.to_s if commits.count > 0
+   org.repositories.each do |r|
+    commits = r.commits_from_author(author)
+    puts r.name + ': ' + commits.count.to_s if commits.count > 0
   end
 end
